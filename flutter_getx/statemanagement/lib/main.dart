61bcdb42c83a24d42dbc92c36229be732a088b37
RISHABH <rishabhchaurasiya20899@gmail.com>
2022-12-28 20:15:38 +0530

    done

--- flutter_getx/statemanagement/lib/main.dart
+++ flutter_getx/statemanagement/lib/main.dart
@@ -16,7 +16,6 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
       ),
       home: Container(color: Colors.white,),
